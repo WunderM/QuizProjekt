a9243452baddb22a062a7b0bbdda9b432102ec44
MoGeCSWi <m.gershoff@web.de>
2025-01-26 12:21:43 +0100

    UI-Überarbeitet

---
+++ CategoriePage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace SoftwareEngineering
+{
+    public partial class CategoryPage : Page
+    {
+        private readonly ApiClient _apiClient;
+
+        public CategoryPage()
+        {
+            InitializeComponent();
+            _apiClient = new ApiClient();
+            LoadCategories();
+        }
+
+        private async void LoadCategories()
+        {
+            // Kategorien abrufen
+            List<string> categories = await _apiClient.GetCategoriesAsync();
+
+            // Kategorien in der ListBox anzeigen
+            foreach (var category in categories)
+            {
+                CategoryList.Items.Add(category);
+            }
+        }
+    }
+}
